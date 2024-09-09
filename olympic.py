055362201c2f2e949752ad0abea18ff1a799e854
Shabroski <Dandi.dsd2@gmail.com>
2024-09-09 19:11:31 -0400

    Add files via upload

---
+++ olympic.py
@@ -0,0 +1,35 @@
+import turtle
+
+tr = turtle.Turtle()
+
+tr.pensize(5)
+
+tr.color("blue")
+tr.penup()
+tr.goto(-110, -25)
+tr.pendown()
+tr.circle(45)
+
+tr.color("black")
+tr.penup()
+tr.goto(0, -25)
+tr.pendown()
+tr.circle(45)
+
+tr.color("red")
+tr.penup()
+tr.goto(110, -25)
+tr.pendown()
+tr.circle(45)
+
+tr.color("yellow")
+tr.penup()
+tr.goto(-55, -75)
+tr.pendown()
+tr.circle(45)
+
+tr.color("green")
+tr.penup()
+tr.goto(55, -75)
+tr.pendown()
+tr.circle(45)
